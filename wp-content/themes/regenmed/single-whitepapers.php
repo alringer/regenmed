815d1714d955359179e02946128e69d1f2dd47f7
Jorge Marin (Mac) <jmarin@seamgen.com>
2019-07-02 17:35:09 -0700

    White Papers

---
+++ wp-content/themes/regenmed/single-whitepapers.php
@@ -0,0 +1,43 @@
+<?php
+
+get_header("light");
+?>
+
+<section id="primary" class="content-area">
+    <main id="main" class="site-main">
+    <?php 
+        if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <article class="rgn-white-paper">
+            <div class="rgn-white-paper__banner"></div>
+            <div class="rgn-white-paper__container">
+                <header class="rgn-white-paper__header">
+                    <h1 class="rgn-white-paper__title"><?php the_title() ?><h1>
+                    <div class="rgn-white-paper__subtitle">
+                        <p class="rgn-white-paper__author"><?php echo 'Published by '; the_author();?></p>
+                        <p class="rgn-white-paper__author"><?php the_date();?></p>
+                    </div>
+                </header>
+                <div class="rgn-white-paper__body">                    
+                    <div class="rgn-white-paper__abstract">
+                        <h3 class="rgn-white-paper__abstract__title">Abstract</h3>
+                        <?php the_content();?>
+                        <button class="rgn-white-paper__download rgn-button">DOWNLOAD WHITE PAPER</button>
+                    </div>
+                </div>
+            </div>
+        </article>
+        <?php endwhile; ?>
+        <?php else: ?>
+            <!-- article -->
+            <article>
+                <h1><?php _e( 'Sorry, nothing to displayHEHE.', 'html5blank' ); ?></h1>
+            </article>
+            <!-- /article -->
+
+        <?php endif; ?>
+    </main>
+</section>
+
+
+<?php
+get_footer();
