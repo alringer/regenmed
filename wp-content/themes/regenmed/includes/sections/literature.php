123aa117d83cf28e2ccfa0e84bba6a9fb6b6c944
Jorge Marin (Mac) <jmarin@seamgen.com>
2019-07-12 17:00:22 -0700

    -Literature section all article is clickable -Contact inputs background -Default desktop backgrounds

--- wp-content/themes/regenmed/includes/sections/literature.php
+++ wp-content/themes/regenmed/includes/sections/literature.php
@@ -22,40 +22,44 @@ function get_post_type_text($type){
             $post0 = get_post($primaryNav[0]->object_id)
         ?>
             <article class="rgn-literature__article rgn-literature__article--main rgn-appear-animation">
-                <div class="rgn-literature__article__image" style="background-image:url('<?php echo get_the_post_thumbnail_url($post0->ID)?>')"></div>
-                <div class="rgn-literature__article__info">
-                    <div class="rgn-literature__article__type">
-                        <?php echo get_post_type_text($post0->post_type); ?>
+                <a class="rgn-literature__article__anchor" href="<?php echo get_post_permalink($post0->ID); ?>">
+                    <div class="rgn-literature__article__image" style="background-image:url('<?php echo get_the_post_thumbnail_url($post0->ID)?>')"></div>
+                    <div class="rgn-literature__article__info">
+                        <div class="rgn-literature__article__type">
+                            <?php echo get_post_type_text($post0->post_type); ?>
+                        </div>
+                        <div class="rgn-literature__article__title"><?php echo $post0->post_title ?></div>
+                        <div class="rgn-literature__article__description">
+                        <?php 
+                        if (strlen($post0->post_excerpt) >= $max_chars)
+                            echo substr($post0->post_excerpt , 0, $max_chars). "...";
+                        else
+                            echo $post0->post_excerpt;
+                        ?>
                     </div>
-                    <div class="rgn-literature__article__title"><a href="<?php echo get_post_permalink($post0->ID); ?>"><?php echo $post0->post_title ?></a></div>
-                    <div class="rgn-literature__article__description">
-                    <?php 
-                    if (strlen($post0->post_excerpt) >= $max_chars)
-                        echo substr($post0->post_excerpt , 0, $max_chars). "...";
-                    else
-                        echo $post0->post_excerpt;
-                    ?>
-                </div>
-                </div>
+                    </div>
+                </a>
             </article>
         <?php } ?>
         <?php if($primaryNav[1]){ 
             $post1 = get_post($primaryNav[1]->object_id)
         ?>
         <article class="rgn-literature__article rgn-literature__article--white-image rgn-literature__article--white-image--reverse rgn-appear-animation">
-            <div class="rgn-literature__article__image" style="background-image:url('<?php echo get_the_post_thumbnail_url($post1->ID)?>')"></div>
-            <div class="rgn-literature__article__info">
-                <div class="rgn-literature__article__type"><?php echo get_post_type_text($post1->post_type); ?></div>
-                <div class="rgn-literature__article__title"><a href="<?php echo get_post_permalink($post1->ID); ?>"><?php echo $post1->post_title ?></a></div>
-                <div class="rgn-literature__article__description">
-                    <?php 
-                    if (strlen($post1->post_excerpt) >= $max_chars)
-                        echo substr($post1->post_excerpt , 0, $max_chars). "...";
-                    else
-                        echo $post1->post_excerpt;
-                    ?>
+            <a class="rgn-literature__article__anchor" href="<?php echo get_post_permalink($post1->ID); ?>">
+                <div class="rgn-literature__article__image" style="background-image:url('<?php echo get_the_post_thumbnail_url($post1->ID)?>')"></div>
+                <div class="rgn-literature__article__info">
+                    <div class="rgn-literature__article__type"><?php echo get_post_type_text($post1->post_type); ?></div>
+                    <div class="rgn-literature__article__title"><?php echo $post1->post_title ?></div>
+                    <div class="rgn-literature__article__description">
+                        <?php 
+                        if (strlen($post1->post_excerpt) >= $max_chars)
+                            echo substr($post1->post_excerpt , 0, $max_chars). "...";
+                        else
+                            echo $post1->post_excerpt;
+                        ?>
+                    </div>
                 </div>
-            </div>
+            </a>
         </article>
         <?php } ?>
         
@@ -65,56 +69,62 @@ function get_post_type_text($type){
             $post2 = get_post($primaryNav[2]->object_id)
         ?>
             <article class="rgn-literature__article rgn-literature__article--white-image rgn-appear-animation">
-                <div class="rgn-literature__article__image rgn-tablet-start" style="background-image:url('<?php echo get_the_post_thumbnail_url($post2->ID)?>')"></div>
-                <div class="rgn-literature__article__info">
-                    <div class="rgn-literature__article__type"><?php echo get_post_type_text($post2->post_type); ?></div>
-                    <div class="rgn-literature__article__title"><a href="<?php echo get_post_permalink($post2->ID);?>"><?php echo $post2->post_title ?></a></div>
-                    <div class="rgn-literature__article__description">
-                    <?php 
-                    if (strlen($post2->post_excerpt) >= $max_chars)
-                        echo substr($post2->post_excerpt , 0, $max_chars). "...";
-                    else
-                        echo $post2->post_excerpt;
-                    ?>
-                </div>
-                </div>
+                <a class="rgn-literature__article__anchor" href="<?php echo get_post_permalink($post2->ID);?>">
+                    <div class="rgn-literature__article__image rgn-tablet-start" style="background-image:url('<?php echo get_the_post_thumbnail_url($post2->ID)?>')"></div>
+                    <div class="rgn-literature__article__info">
+                        <div class="rgn-literature__article__type"><?php echo get_post_type_text($post2->post_type); ?></div>
+                        <div class="rgn-literature__article__title"><?php echo $post2->post_title ?></div>
+                        <div class="rgn-literature__article__description">
+                        <?php 
+                        if (strlen($post2->post_excerpt) >= $max_chars)
+                            echo substr($post2->post_excerpt , 0, $max_chars). "...";
+                        else
+                            echo $post2->post_excerpt;
+                        ?>
+                    </div>
+                    </div>
+                </a>
             </article>
         <?php } ?>
         <?php if($primaryNav[3]){ 
             $post3 = get_post($primaryNav[3]->object_id)
         ?>
             <article class="rgn-literature__article rgn-literature__article--black rgn-appear-animation">
-                <div class="rgn-literature__article__info">
-                    <div class="rgn-literature__article__type"><?php echo get_post_type_text($post3->post_type); ?></div>
-                    <div class="rgn-literature__article__title"><a href="<?php echo get_post_permalink($post3->ID); ?>"><?php echo $post3->post_title ?></a></div>
-                    <div class="rgn-literature__article__description">
-                    <?php 
-                        if (strlen($post3->post_excerpt) >= $max_chars)
-                            echo substr($post3->post_excerpt , 0, $max_chars). "...";
-                        else
-                            echo $post3->post_excerpt;
-                        ?>
+                <a class="rgn-literature__article__anchor" href="<?php echo get_post_permalink($post3->ID); ?>">
+                    <div class="rgn-literature__article__info">
+                        <div class="rgn-literature__article__type"><?php echo get_post_type_text($post3->post_type); ?></div>
+                        <div class="rgn-literature__article__title"><?php echo $post3->post_title ?></div>
+                        <div class="rgn-literature__article__description">
+                        <?php 
+                            if (strlen($post3->post_excerpt) >= $max_chars)
+                                echo substr($post3->post_excerpt , 0, $max_chars). "...";
+                            else
+                                echo $post3->post_excerpt;
+                            ?>
+                        </div>
                     </div>
-                </div>
+                </a>
             </article>
         <?php } ?>
         <?php if($primaryNav[4]){ 
             $post4 = get_post($primaryNav[4]->object_id)
         ?>
         <article class="rgn-literature__article rgn-literature__article--white-image rgn-literature__article--white-image--reverse rgn-appear-animation">
-            <div class="rgn-literature__article__image rgn-tablet-start" style="background-image:url('<?php echo get_the_post_thumbnail_url($post4->ID)?>')"></div>
-            <div class="rgn-literature__article__info">
-                <div class="rgn-literature__article__type"><?php echo get_post_type_text($post4->post_type);?></div>
-                <div class="rgn-literature__article__title"><a href="<?php echo get_post_permalink($post4->ID); ?>"><?php echo $post4->post_title ?></a></div>
-                <div class="rgn-literature__article__description">
-                    <?php 
-                    if (strlen($post4->post_excerpt) >= $max_chars)
-                        echo substr($post4->post_excerpt , 0, $max_chars). "...";
-                    else
-                        echo $post4->post_excerpt;
-                    ?>
+            <a class="rgn-literature__article__anchor" href="<?php echo get_post_permalink($post4->ID); ?>">
+                <div class="rgn-literature__article__image rgn-tablet-start" style="background-image:url('<?php echo get_the_post_thumbnail_url($post4->ID)?>')"></div>
+                <div class="rgn-literature__article__info">
+                    <div class="rgn-literature__article__type"><?php echo get_post_type_text($post4->post_type);?></div>
+                    <div class="rgn-literature__article__title"><?php echo $post4->post_title ?></div>
+                    <div class="rgn-literature__article__description">
+                        <?php 
+                        if (strlen($post4->post_excerpt) >= $max_chars)
+                            echo substr($post4->post_excerpt , 0, $max_chars). "...";
+                        else
+                            echo $post4->post_excerpt;
+                        ?>
+                    </div>
                 </div>
-            </div>
+            </a>
         </article>
         <?php } ?>
     </div>
